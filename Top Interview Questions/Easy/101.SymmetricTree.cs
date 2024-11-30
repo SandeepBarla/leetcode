5093a9ca1482a4fe5e6c281274f26316321d9bc7
Sandeep Barla <barlavenkatsandeep@gmail.com>
2024-11-30 00:42:54 -0600

    add recursive approach for 101 solution

---
+++ Top Interview Questions/Easy/101.SymmetricTree.cs
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+// Recursive Approach
+public class Solution {
+    public bool IsSymmetric(TreeNode root) {
+        return CheckSymmetric(root.left, root.right); // recursive method to verify the mirror image of a node
+    }
+
+    private bool CheckSymmetric(TreeNode left, TreeNode right){
+        // we always verify the left most and right most elements
+        if(left == null || right == null) return left == right;
+        return (left.val == right.val) && CheckSymmetric(left.left, right.right) & CheckSymmetric(left.right, right.left);
+    }
+}
