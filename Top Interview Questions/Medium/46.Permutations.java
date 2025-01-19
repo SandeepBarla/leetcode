45344bdce70bb16e1d9ae37a292fca1082b63f2c
Sandeep Barla <barlavenkatsandeep@gmail.com>
2025-01-18 18:26:54 -0600

    add 48 solution

--- Top Interview Questions/Medium/46.Permutations.java
+++ Top Interview Questions/Medium/46.Permutations.java
@@ -1,3 +1,6 @@
+// Backtracking solution with maintaining visited elements
+// T.C = O(n*n!); n is length of the array nums
+// S.C = O(n*n!); n is length of the array nums
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
@@ -7,23 +10,26 @@ class Solution {
     }
 
     private void backtrack(int[] nums, List<List<Integer>> result, List<Integer> comb, boolean[] visited){
+        // if combination has length same as nums array, add it to result and return
         if(comb.size()==nums.length){
             result.add(new ArrayList<>(comb));
             return;
         }
-
+        // iterate through all the elements of nums array, and add only unvisited elements to combination
         for(int i=0; i<nums.length; i++){
-            if(visited[i]) continue;
-            visited[i] = true;
-            comb.add(nums[i]);
-            backtrack(nums, result, comb, visited);
-            comb.remove(Integer.valueOf(nums[i]));
-            visited[i] = false;
+            if(visited[i]) continue; // check if the element was vistied already
+            visited[i] = true; // mark it as visited
+            comb.add(nums[i]); // add it to combination
+            backtrack(nums, result, comb, visited); // perform backtrack on the combination
+            comb.remove(Integer.valueOf(nums[i])); // remove the element from combination
+            visited[i] = false; // mark it as unvisited
         }
     }
 }
 
 // Backtracking solution
+// T.C = O(n*n!); n is length of the array nums
+// S.C = O(n*n!); n is length of the array nums
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
---
+++ Top Interview Questions/Medium/48.RotateImage.java
@@ -0,0 +1,23 @@
+// T.C = O(n^2)
+// S.C = O(1)
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        // transpose the matrix
+        for(int i=0; i<n; i++){
+            for(int j=i+1; j<n; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        // reverse each row
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n/2; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n-1-j];
+                matrix[i][n-1-j] = temp;
+            }
+        }
+    }
+}
