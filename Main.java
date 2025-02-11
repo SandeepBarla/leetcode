ab8e37a3fc07bc8ac82b7ee673301580925b50ff
Sandeep Barla <barlavenkatsandeep@gmail.com>
2025-02-10 19:01:55 -0600

    add 98 solution with treenode and treeutils

--- Main.java
+++ Main.java
@@ -1,10 +1,10 @@
 import common.Solution;
-import solutions.top_interview_questions.medium.LC_91_DecodeWays;
+import solutions.top_interview_questions.medium.LC_98_ValidateBST;
 
 public class Main {
     public static void main(String[] args) {
         // Change this line to test any solution
-        Solution solution = new LC_91_DecodeWays(); // Replace with the class you want to run
+        Solution solution = new LC_98_ValidateBST(); // Replace with the class you want to run
 
         System.out.println("\n--- Running " + solution.getClass().getSimpleName() + " ---");
         solution.run();
