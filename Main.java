5b0fd05322f10e877a3eef1b0acefc7b33771e54
Sandeep Barla <barlavenkatsandeep@gmail.com>
2025-02-14 17:36:00 -0600

    add 128 solution

--- Main.java
+++ Main.java
@@ -1,11 +1,11 @@
 import common.Solution;
-import solutions.top_interview_questions.medium.LC_122_BestTimeToBuyAndSellStockII;
+import solutions.top_interview_questions.medium.LC_128_LongestConsecutiveSequence;
 
 public class Main {
     public static void main(String[] args) {
         // Change this line to test any solution
-        Solution solution = new LC_122_BestTimeToBuyAndSellStockII(); // Replace with the class
-                                                                      // you want
+        Solution solution = new LC_128_LongestConsecutiveSequence(); // Replace with the class
+                                                                     // you want
         // to run
 
         System.out.println("\n--- Running " + solution.getClass().getSimpleName() + " ---");
