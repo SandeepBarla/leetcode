203d96d5ac3ea396ea6ad7894ac5843c79a9347c
Sandeep Barla <barlavenkatsandeep@gmail.com>
2025-03-11 10:24:42 -0500

    add 166, 209 solutions

--- Main.java
+++ Main.java
@@ -1,10 +1,10 @@
 import common.Solution;
-import solutions.company_wise.goldman_sachs.medium.LC_289_GameOfLife;
+import solutions.company_wise.goldman_sachs.medium.LC_209_MinSubArrayLen;
 
 public class Main {
     public static void main(String[] args) {
         // Change this line to test any solution
-        Solution solution = new LC_289_GameOfLife(); // Replace with the class
+        Solution solution = new LC_209_MinSubArrayLen(); // Replace with the class
         // you want
         // to run
 
