0eb2272772afae8f30615ca06d328cd03aba0b8d
Sandeep Barla <barlavenkatsandeep@gmail.com>
2025-03-16 23:58:45 -0500

    add 2206 daily challenge solution

--- Main.java
+++ Main.java
@@ -1,10 +1,10 @@
 import common.Solution;
-import solutions.pareto_problem_set.medium.LC_230_KthSmallestElementInBST;
+import solutions.easy.LC_2206_DivideArrayIntoEqualPairs;
 
 public class Main {
     public static void main(String[] args) {
         // Change this line to test any solution
-        Solution solution = new LC_230_KthSmallestElementInBST(); // Replace with the class you want to run
+        Solution solution = new LC_2206_DivideArrayIntoEqualPairs(); // Replace with the class you want to run
 
         System.out.println("\n--- Running " + solution.getClass().getSimpleName() + " ---");
         solution.run();
