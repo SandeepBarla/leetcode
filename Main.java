6faf7e5e3ddff3c48e0f76810878f608157b551b
Sandeep Barla <barlavenkatsandeep@gmail.com>
2025-03-17 23:29:04 -0500

    add 226 solution

--- Main.java
+++ Main.java
@@ -1,10 +1,10 @@
 import common.Solution;
-import solutions.easy.LC_2206_DivideArrayIntoEqualPairs;
+import solutions.pareto_problem_set.easy.LC_226_InvertBinaryTree;
 
 public class Main {
     public static void main(String[] args) {
         // Change this line to test any solution
-        Solution solution = new LC_2206_DivideArrayIntoEqualPairs(); // Replace with the class you want to run
+        Solution solution = new LC_226_InvertBinaryTree(); // Replace with the class you want to run
 
         System.out.println("\n--- Running " + solution.getClass().getSimpleName() + " ---");
         solution.run();
