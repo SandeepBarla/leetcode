ddcb300f713ce135274233d85687ff337ade3d31
Sandeep Barla <barlavenkatsandeep@gmail.com>
2025-03-21 18:03:10 -0500

    add 207, 695 and 994 solutions

--- Main.java
+++ Main.java
@@ -1,10 +1,10 @@
 import common.Solution;
-import solutions.pareto_problem_set.easy.LC_226_InvertBinaryTree;
+import solutions.company_wise.amazon.medium.LC_994_RottingOranges;
 
 public class Main {
     public static void main(String[] args) {
         // Change this line to test any solution
-        Solution solution = new LC_226_InvertBinaryTree(); // Replace with the class you want to run
+        Solution solution = new LC_994_RottingOranges(); // Replace with the class you want to run
 
         System.out.println("\n--- Running " + solution.getClass().getSimpleName() + " ---");
         solution.run();
