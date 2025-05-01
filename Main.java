2be4661b1631978ceaf4e632c8d7fb81bde2a891
Sandeep Barla <barlavenkatsandeep@gmail.com>
2025-05-01 16:08:13 -0500

    add 19 solution

--- Main.java
+++ Main.java
@@ -1,10 +1,10 @@
 import common.Solution;
-import solutions.top_interview_questions.easy.LC_21_MergeTwoSortedLists;
+import solutions.pareto_problem_set.medium.LC_19_RemoveNthNodeFromEndOfList_Optimized;
 
 public class Main {
     public static void main(String[] args) {
         // Change this line to test any solution
-        Solution solution = new LC_21_MergeTwoSortedLists(); // Replace with the class you want to run
+        Solution solution = new LC_19_RemoveNthNodeFromEndOfList_Optimized(); // Replace with the class you want to run
 
         System.out.println("\n--- Running " + solution.getClass().getSimpleName() + " ---");
         solution.run();
