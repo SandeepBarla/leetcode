a3e50e4f9b4f312432b55ef6d280a74b0c192e77
Sandeep Barla <barlavenkatsandeep@gmail.com>
2025-04-30 23:10:09 -0500

    update 21 solution

--- Main.java
+++ Main.java
@@ -1,10 +1,10 @@
 import common.Solution;
-import solutions.top_interview_questions.easy.LC_20_ValidParentheses;
+import solutions.top_interview_questions.easy.LC_21_MergeTwoSortedLists;
 
 public class Main {
     public static void main(String[] args) {
         // Change this line to test any solution
-        Solution solution = new LC_20_ValidParentheses(); // Replace with the class you want to run
+        Solution solution = new LC_21_MergeTwoSortedLists(); // Replace with the class you want to run
 
         System.out.println("\n--- Running " + solution.getClass().getSimpleName() + " ---");
         solution.run();
