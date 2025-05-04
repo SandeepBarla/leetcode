b0b89c62818d36fa4344e8fcec30dc066ec56d1c
Sandeep Barla <barlavenkatsandeep@gmail.com>
2025-05-04 10:53:10 -0500

    add 235 solution

--- Main.java
+++ Main.java
@@ -1,10 +1,10 @@
 import common.Solution;
-import solutions.pareto_problem_set.easy.LC_572_SubtreeOfAnotherTree;
+import solutions.pareto_problem_set.medium.LC_235_LowestCommonAncestorBST;
 
 public class Main {
     public static void main(String[] args) {
         // Change this line to test any solution
-        Solution solution = new LC_572_SubtreeOfAnotherTree(); // Replace with the class you want to run
+        Solution solution = new LC_235_LowestCommonAncestorBST(); // Replace with the class you want to run
 
         System.out.println("\n--- Running " + solution.getClass().getSimpleName() + " ---");
         solution.run();
