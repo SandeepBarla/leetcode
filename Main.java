687960012f8b63bcdca2ee273ccd8d3698341ea2
Sandeep Barla <barlavenkatsandeep@gmail.com>
2025-05-04 13:05:12 -0500

    add 703 solution

--- Main.java
+++ Main.java
@@ -1,10 +1,10 @@
 import common.Solution;
-import solutions.pareto_problem_set.medium.LC_199_BinaryTreeRightSideView;
+import solutions.pareto_problem_set.easy.LC_703_KthLargestElementStream;
 
 public class Main {
     public static void main(String[] args) {
         // Change this line to test any solution
-        Solution solution = new LC_199_BinaryTreeRightSideView(); // Replace with the class you want to run
+        Solution solution = new LC_703_KthLargestElementStream(); // Replace with the class you want to run
 
         System.out.println("\n--- Running " + solution.getClass().getSimpleName() + " ---");
         solution.run();
