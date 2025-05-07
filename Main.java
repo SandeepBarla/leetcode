37fc884477ae2f47fe5df41c91d756eed4451d5c
Sandeep Barla <barlavenkatsandeep@gmail.com>
2025-05-06 23:55:13 -0500

    test 417 solution

--- Main.java
+++ Main.java
@@ -1,10 +1,10 @@
 import common.Solution;
-import solutions.pareto_problem_set.easy.LC_703_KthLargestElementStream;
+import solutions.pareto_problem_set.medium.LC_417_PacificAtlanticWaterFlow;
 
 public class Main {
     public static void main(String[] args) {
         // Change this line to test any solution
-        Solution solution = new LC_703_KthLargestElementStream(); // Replace with the class you want to run
+        Solution solution = new LC_417_PacificAtlanticWaterFlow(); // Replace with the class you want to run
 
         System.out.println("\n--- Running " + solution.getClass().getSimpleName() + " ---");
         solution.run();
