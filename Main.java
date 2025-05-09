8f3c5a97c911a964418f1b3720eac3c2d95520d0
Sandeep Barla <barlavenkatsandeep@gmail.com>
2025-05-08 23:00:48 -0500

    test 130 solution

--- Main.java
+++ Main.java
@@ -1,10 +1,10 @@
 import common.Solution;
-import solutions.pareto_problem_set.medium.LC_417_PacificAtlanticWaterFlow;
+import solutions.top_interview_questions.medium.LC_130_SurroundedRegions;
 
 public class Main {
     public static void main(String[] args) {
         // Change this line to test any solution
-        Solution solution = new LC_417_PacificAtlanticWaterFlow(); // Replace with the class you want to run
+        Solution solution = new LC_130_SurroundedRegions(); // Replace with the class you want to run
 
         System.out.println("\n--- Running " + solution.getClass().getSimpleName() + " ---");
         solution.run();
