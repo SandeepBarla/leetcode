81c2bc1e10efb111467a1c9c62f419e6dc5f41e9
Sandeep Barla <barlavenkatsandeep@gmail.com>
2025-05-10 19:27:48 -0500

    add 743 solution

--- Main.java
+++ Main.java
@@ -1,10 +1,10 @@
 import common.Solution;
-import solutions.pareto_problem_set.medium.LC_210_CourseScheduleII;
+import solutions.company_wise.google.LC_743_NetworkDelayTime;
 
 public class Main {
     public static void main(String[] args) {
         // Change this line to test any solution
-        Solution solution = new LC_210_CourseScheduleII(); // Replace with the class you want to run
+        Solution solution = new LC_743_NetworkDelayTime(); // Replace with the class you want to run
 
         System.out.println("\n--- Running " + solution.getClass().getSimpleName() + " ---");
         solution.run();
