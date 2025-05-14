27f16cbcc9b38d68d11939962619e09c7f9938c6
Sandeep Barla <barlavenkatsandeep@gmail.com>
2025-05-14 16:56:41 -0500

    add 2096 solution

--- Main.java
+++ Main.java
@@ -1,10 +1,10 @@
 import common.Solution;
-import solutions.company_wise.google.medium.LC_3341_FindMinimumTimeToReachLastRoomI;
+import solutions.company_wise.google.medium.LC_2096_StepByStepDirectionsInBinaryTree;
 
 public class Main {
     public static void main(String[] args) {
         // Change this line to test any solution
-        Solution solution = new LC_3341_FindMinimumTimeToReachLastRoomI(); // Replace with the class you want to run
+        Solution solution = new LC_2096_StepByStepDirectionsInBinaryTree(); // Replace with the class you want to run
 
         System.out.println("\n--- Running " + solution.getClass().getSimpleName() + " ---");
         solution.run();
