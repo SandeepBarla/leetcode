651956c4904674810717aa617dcc6a284556f223
Sandeep Barla <barlavenkatsandeep@gmail.com>
2025-05-20 22:54:59 -0500

    test 746 sol

--- python/main.py
+++ python/main.py
@@ -1,11 +1,11 @@
 # Main entry point to run one LeetCode solution at a time
 
-from medium.lc_43_multiply_strings import LC_43_MultiplyStrings
+from easy.lc_746_min_cost_climbing_stairs import LC_746_MinCostClimbingStairs
 from solution_base import Solution
 
 if __name__ == "__main__":
     # ✅ Instantiate the solution you want to test
-    solution: Solution = LC_43_MultiplyStrings()  # ✅ Change this line
+    solution: Solution = LC_746_MinCostClimbingStairs()  # ✅ Change this line
 
     print(f"\n--- Running {solution.__class__.__name__} ---")
     solution.run()
