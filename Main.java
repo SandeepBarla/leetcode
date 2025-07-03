027545c2839d1c46a3817dffab55adc5b2b143be
Sandeep Barla <barlavenkatsandeep@gmail.com>
2025-07-03 14:03:04 -0500

    add 973 sol

--- Main.java
+++ Main.java
@@ -1,10 +1,10 @@
 import common.Solution;
-import solutions.neetcode_150.medium.LC_134_GasStation;
+import solutions.neetcode_150.medium.LC_973_KClosestPointsToOrigin;
 
 public class Main {
     public static void main(String[] args) {
         // Change this line to test any solution
-        Solution solution = new LC_134_GasStation(); // Replace with the class you want to run
+        Solution solution = new LC_973_KClosestPointsToOrigin(); // Replace with the class you want to run
 
         System.out.println("\n--- Running " + solution.getClass().getSimpleName() + " ---");
         solution.run();
