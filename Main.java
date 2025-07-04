34b3bb2efb619a77b7c23d5cb54515cabad29f85
Sandeep Barla <barlavenkatsandeep@gmail.com>
2025-07-03 21:04:44 -0500

    add 152 sol

--- Main.java
+++ Main.java
@@ -1,10 +1,10 @@
 import common.Solution;
-import solutions.neetcode_150.medium.LC_973_KClosestPointsToOrigin;
+import solutions.neetcode_150.medium.LC_152_MaximumProductSubarray;
 
 public class Main {
     public static void main(String[] args) {
         // Change this line to test any solution
-        Solution solution = new LC_973_KClosestPointsToOrigin(); // Replace with the class you want to run
+        Solution solution = new LC_152_MaximumProductSubarray(); // Replace with the class you want to run
 
         System.out.println("\n--- Running " + solution.getClass().getSimpleName() + " ---");
         solution.run();
