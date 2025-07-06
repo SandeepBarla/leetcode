3fb34fb90a938a11e7e4a3d01a4497a19efd7e6a
Sandeep Barla <barlavenkatsandeep@gmail.com>
2025-07-06 09:59:58 -0500

    add 57 sol

--- Main.java
+++ Main.java
@@ -1,10 +1,10 @@
 import common.Solution;
-import solutions.neetcode_150.medium.LC_139_WordBreak;
+import solutions.neetcode_150.medium.LC_57_InsertInterval;
 
 public class Main {
     public static void main(String[] args) {
         // Change this line to test any solution
-        Solution solution = new LC_139_WordBreak(); // Replace with the class you want to run
+        Solution solution = new LC_57_InsertInterval(); // Replace with the class you want to run
 
         System.out.println("\n--- Running " + solution.getClass().getSimpleName() + " ---");
         solution.run();
