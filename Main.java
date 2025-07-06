bcae2fd6ea1e431debeb7c58afa29eaa4edfc519
Sandeep Barla <barlavenkatsandeep@gmail.com>
2025-07-05 21:28:47 -0500

    add 139 sol

--- Main.java
+++ Main.java
@@ -1,10 +1,10 @@
 import common.Solution;
-import solutions.neetcode_150.medium.LC_435_NonOverlappingIntervals;
+import solutions.neetcode_150.medium.LC_139_WordBreak;
 
 public class Main {
     public static void main(String[] args) {
         // Change this line to test any solution
-        Solution solution = new LC_435_NonOverlappingIntervals(); // Replace with the class you want to run
+        Solution solution = new LC_139_WordBreak(); // Replace with the class you want to run
 
         System.out.println("\n--- Running " + solution.getClass().getSimpleName() + " ---");
         solution.run();
