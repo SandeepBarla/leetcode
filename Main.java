7734c47c6d694669b25937fe9b456b8d8635eb00
Sandeep Barla <barlavenkatsandeep@gmail.com>
2025-07-06 23:50:46 -0500

    add 1143 sol

--- Main.java
+++ Main.java
@@ -1,10 +1,10 @@
 import common.Solution;
-import solutions.neetcode_150.medium.LC_57_InsertInterval;
+import solutions.neetcode_150.medium.LC_1143_LongestCommonSubsequence;
 
 public class Main {
     public static void main(String[] args) {
         // Change this line to test any solution
-        Solution solution = new LC_57_InsertInterval(); // Replace with the class you want to run
+        Solution solution = new LC_1143_LongestCommonSubsequence(); // Replace with the class you want to run
 
         System.out.println("\n--- Running " + solution.getClass().getSimpleName() + " ---");
         solution.run();
