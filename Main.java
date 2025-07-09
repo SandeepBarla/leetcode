793a22df9491309bb6df4e7ec54311f16ad812d7
Sandeep Barla <barlavenkatsandeep@gmail.com>
2025-07-09 08:05:06 -0500

    add 355 sol

--- Main.java
+++ Main.java
@@ -1,10 +1,10 @@
 import common.Solution;
-import solutions.neetcode_150.medium.LC_40_CombinationSumII;
+import solutions.neetcode_150.medium.LC_355_DesignTwitter;
 
 public class Main {
     public static void main(String[] args) {
         // Change this line to test any solution
-        Solution solution = new LC_40_CombinationSumII(); // Replace with the class you want to run
+        Solution solution = new LC_355_DesignTwitter(); // Replace with the class you want to run
 
         System.out.println("\n--- Running " + solution.getClass().getSimpleName() + " ---");
         solution.run();
