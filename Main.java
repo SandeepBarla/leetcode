00e5cf8506f3753039eb4b3af9f6d2bd0d704d7a
Sandeep Barla <barlavenkatsandeep@gmail.com>
2025-07-10 19:57:48 -0500

    add 621 sol

--- Main.java
+++ Main.java
@@ -1,10 +1,10 @@
 import common.Solution;
-import solutions.neetcode_150.medium.LC_2013_DetectSquares;
+import solutions.neetcode_150.medium.LC_621_TaskScheduler;
 
 public class Main {
     public static void main(String[] args) {
         // Change this line to test any solution
-        Solution solution = new LC_2013_DetectSquares(); // Replace with the class you want to run
+        Solution solution = new LC_621_TaskScheduler(); // Replace with the class you want to run
 
         System.out.println("\n--- Running " + solution.getClass().getSimpleName() + " ---");
         solution.run();
