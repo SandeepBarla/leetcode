c409078fb3952efe755637be06437e0a97268cf1
Sandeep Barla <barlavenkatsandeep@gmail.com>
2025-07-11 09:44:49 -0500

    add 416 sol

--- Main.java
+++ Main.java
@@ -1,10 +1,10 @@
 import common.Solution;
-import solutions.neetcode_150.medium.LC_621_TaskScheduler;
+import solutions.neetcode_150.medium.LC_416_PartitionEqualSubsetSum;
 
 public class Main {
     public static void main(String[] args) {
         // Change this line to test any solution
-        Solution solution = new LC_621_TaskScheduler(); // Replace with the class you want to run
+        Solution solution = new LC_416_PartitionEqualSubsetSum(); // Replace with the class you want to run
 
         System.out.println("\n--- Running " + solution.getClass().getSimpleName() + " ---");
         solution.run();
