2b00578f1f8844ca8bf2f0afe3c51c9a97bc214b
Sandeep Barla <barlavenkatsandeep@gmail.com>
2025-07-12 08:47:39 -0500

    add 647 sol

--- Main.java
+++ Main.java
@@ -1,10 +1,10 @@
 import common.Solution;
-import solutions.neetcode_150.medium.LC_416_PartitionEqualSubsetSum;
+import solutions.neetcode_150.medium.LC_647_PalindromicSubstrings;
 
 public class Main {
     public static void main(String[] args) {
         // Change this line to test any solution
-        Solution solution = new LC_416_PartitionEqualSubsetSum(); // Replace with the class you want to run
+        Solution solution = new LC_647_PalindromicSubstrings(); // Replace with the class you want to run
 
         System.out.println("\n--- Running " + solution.getClass().getSimpleName() + " ---");
         solution.run();
