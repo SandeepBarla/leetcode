58e014d414180c0a53a0f37ea77afbc28c0157af
Sandeep Barla <barlavenkatsandeep@gmail.com>
2025-07-12 20:49:49 -0500

    add 72 and 300 sol

--- Main.java
+++ Main.java
@@ -1,10 +1,10 @@
 import common.Solution;
-import solutions.neetcode_150.medium.LC_647_PalindromicSubstrings;
+import solutions.neetcode_150.medium.LC_72_EditDistance;
 
 public class Main {
     public static void main(String[] args) {
         // Change this line to test any solution
-        Solution solution = new LC_647_PalindromicSubstrings(); // Replace with the class you want to run
+        Solution solution = new LC_72_EditDistance(); // Replace with the class you want to run
 
         System.out.println("\n--- Running " + solution.getClass().getSimpleName() + " ---");
         solution.run();
