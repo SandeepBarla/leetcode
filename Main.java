9aa2e4a712fa016765de2cc1008f490d45cfabbc
Sandeep Barla <barlavenkatsandeep@gmail.com>
2025-07-16 12:26:39 -0500

    add 920 sol

--- Main.java
+++ Main.java
@@ -1,10 +1,10 @@
 import common.Solution;
-import solutions.neetcode_150.medium.LC_271_EncodeDecodeStrings;
+import solutions.neetcode_150.easy.LC_920_CanAttendMeetings;
 
 public class Main {
     public static void main(String[] args) {
         // Change this line to test any solution
-        Solution solution = new LC_271_EncodeDecodeStrings(); // Replace with the class you want to run
+        Solution solution = new LC_920_CanAttendMeetings(); // Replace with the class you want to run
 
         System.out.println("\n--- Running " + solution.getClass().getSimpleName() + " ---");
         solution.run();
