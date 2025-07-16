d84f9dd4acdec17721e49ca8b1841cfcedf55e8f
Sandeep Barla <barlavenkatsandeep@gmail.com>
2025-07-16 13:09:08 -0500

    add 295 sol

--- Main.java
+++ Main.java
@@ -1,10 +1,10 @@
 import common.Solution;
-import solutions.neetcode_150.easy.LC_920_CanAttendMeetings;
+import solutions.neetcode_150.hard.LC_295_FindMedianFromDataStream;
 
 public class Main {
     public static void main(String[] args) {
         // Change this line to test any solution
-        Solution solution = new LC_920_CanAttendMeetings(); // Replace with the class you want to run
+        Solution solution = new LC_295_FindMedianFromDataStream(); // Replace with the class you want to run
 
         System.out.println("\n--- Running " + solution.getClass().getSimpleName() + " ---");
         solution.run();
