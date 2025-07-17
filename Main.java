03d7b7495b9e4c777445b03d81d89fc78c509189
Sandeep Barla <barlavenkatsandeep@gmail.com>
2025-07-17 08:03:42 -0500

    add 124 sol

--- Main.java
+++ Main.java
@@ -1,10 +1,10 @@
 import common.Solution;
-import solutions.neetcode_150.hard.LC_295_FindMedianFromDataStream;
+import solutions.neetcode_150.hard.LC_124_BinaryTreeMaximumPathSum;
 
 public class Main {
     public static void main(String[] args) {
         // Change this line to test any solution
-        Solution solution = new LC_295_FindMedianFromDataStream(); // Replace with the class you want to run
+        Solution solution = new LC_124_BinaryTreeMaximumPathSum(); // Replace with the class you want to run
 
         System.out.println("\n--- Running " + solution.getClass().getSimpleName() + " ---");
         solution.run();
