758bee8b168b98319640e7eea70eb61b3d239a64
Sandeep Barla <barlavenkatsandeep@gmail.com>
2025-07-20 14:19:47 -0500

    add 323 sol

--- Main.java
+++ Main.java
@@ -1,10 +1,11 @@
 import common.Solution;
-import solutions.neetcode_150.easy.LC_338_CountingBits;
+import solutions.neetcode_150.medium.LC_323_NumberOfConnectedComponentsInAnUndirectedGraph;
 
 public class Main {
     public static void main(String[] args) {
         // Change this line to test any solution
-        Solution solution = new LC_338_CountingBits(); // Replace with the class you want to run
+        Solution solution = new LC_323_NumberOfConnectedComponentsInAnUndirectedGraph(); // Replace with the class you
+                                                                                         // want to run
 
         System.out.println("\n--- Running " + solution.getClass().getSimpleName() + " ---");
         solution.run();
